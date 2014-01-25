23a9c793a62c5b5eb962467a670b4a41e2360091
Aaron <Aaron.Hanich@gmail.com>
2014-01-25 11:23:25 -0700

    making form

--- assignments/1survey.php
+++ assignments/1survey.php
@@ -1,9 +1,12 @@
 <?php
    $timeLimit = 60 * 60 * 24 * 30;
    
-   if(isset($_SESSION['responded']) && $SESSION['responded'])
+   if((isset($_SESSION['responded']) && $_SESSION['responded']) ||
+      ($_POST['response']) ||
+      (isset($_COOKIE['responded']) && $_COOKIE['responded']))
    {  
       setcookie('responded', true, $timeLimit);
+      $_SESSION['responded'] = true;
    }
    
    if(isset($_COOKIE['responded']))
--- assignments/phphelper/survey.php
+++ assignments/phphelper/survey.php
@@ -1,6 +1,28 @@
 <head>
    <title>Survey</title>
+   <script type="text/javascript" src="jshelper/survey.js"></script>
 </head>
 <body>
    <p>This is the Survey</p>
+   <form class="form" onsubmit="return 1SurveyValidate()" action="../1survey.php" method="POST">
+      <p>Name <input type="text" id='name' name="name" onblur="validName(this)"></input></p>
+      <p>Gender <select id='gender' name="gender" onblur="validGender(this)">
+                  <option value="na"> </option>
+                  <option value="ma">Male</option>
+                  <option value="fe">Female</option>
+                </select></p>
+      <p>Do you play League of Legends &nbsp;    yes<input type='radio' name='play' value='yes' checked='checked'/>
+         no<input type='radio' name='play' value='no'/></p>
+      <p>If the above question is yes what positions do you prefer</p>
+      <div class='checkBox'>
+            <input type="checkbox" name="pos[]" value="top">Top<br/>
+				<input type="checkbox" name="pos[]" value="mid">Mid<br/>
+				<input type="checkbox" name="pos[]" value="jg">Jungle<br/>
+				<input type="checkbox" name="pos[]" value="adc">ADC<br/>
+				<input type="checkbox" name="pos[]" value="supp">Support<br/>
+				<input type="checkbox" name="pos[]" value="na">I don't play<br/>
+      </div>
+      <button type='submit' name='respond' value='true'/>
+      <button type='cancel' name='respond' value='false'/>
+   </form>
 </body>
